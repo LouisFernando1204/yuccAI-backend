5e4440080b3147f618e6e5473e81e629f52154b9
Louis Fernando <ferlouis55@gmail.com>
2024-11-21 16:45:20 +0700

    Change CommonJS to ESModules

--- src/server.ts
+++ src/server.ts
@@ -1,17 +1,17 @@
 import express, { Request, Response } from "express";
-import dotenv from 'dotenv';
+import dotenv from "dotenv";
 
 dotenv.config();
 
 const app = express();
-const path = require('path');
 const PORT = process.env.PORT || 3500;
 
-app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-app.get('/', (req: Request, res: Response) => {
-    res.send('Hello Yobel!');
+app.get("/", (req: Request, res: Response) => {
+    res.send("Hello, TypeScript with ES Modules!");
 });
 
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
